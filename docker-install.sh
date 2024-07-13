40aa77aaf0619552fc9e7ad16c6f4c30aa8bfbdd
kashish srivastava <kash.pegasus@gmail.com>
2024-07-13 22:22:49 +0530

    env

--- docker-install.sh
+++ docker-install.sh
@@ -4,4 +4,7 @@ curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo usermod -aG docker kash
 sudo apt install -y libffi-dev libssl-dev python3 python3-pip
-sudo pip3 install docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-`uname -s`-`uname -m` > docker-compose
+sudo mv docker-compose /usr/bin/
+sudo chown root: /usr/bin/docker-compose
+sudo chmod +x /usr/bin/docker-compose
